e290f49640dc1d8e58b339b4e99c3aea036dbfa4
Johannes Aengenheyster <johannes.ae@hotmail.de>
2023-10-05 11:41:20 +0200

    further setting up regression data structure

--- scripts/regression.R
+++ scripts/regression.R
@@ -26,6 +26,7 @@ gd_pmx <- function(dt_pmdb) {
         .[!(museum_status == "closed" & is.na(year_closed))] %>% # yeet closed with missing closing year
         .[!is.na(year_opened)] # yeet all without proper opening year
 
+
     ## dt_pmx[!complete.cases(dt_pmx), .N, museum_status]
     ## dt_pmx[, .N, museum_status]
     
@@ -36,21 +37,37 @@ gd_pmx <- function(dt_pmdb) {
 
     
 
-gd_pmyear <- function(dt_pmdb) {
+gd_pmyear <- function(dt_pmx) {
     gw_fargs(match.call())
     if (as.character(match.call()[[1]]) %in% fstd){browser()}
     1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;
     #' generate dt in pm-year format
 
+
+    dt_pmyear <- dt_pmx[, last_year := fifelse(museum_status == "closed", year_closed, END_YEAR)] %>%
+        ## expand to pm_year UoA
+        .[, .(year = year_opened:last_year), .(ID, iso3c, museum_status, year_opened, year_closed)] %>%
+        ## set closing variable
+        .[, closing := fifelse(museum_status == "closed" & year_closed == year, 1, 0)] %>% 
+        .[!(year > END_YEAR)] %>% # yeet pm years beyond END_YEAR
+        .[!(year_opened > year)] %>% # yeet pm-years where expansion went into negative direction
+        .[, age := year - year_opened]
     
+    ## dt_pmyear[, .N, .(closing, museum_status)] # check closing variable, looks good
+    ## dt_pmyear[year > END_YEAR]
+    ## fnunique(dt_pmyear$ID)
 
+    ## filter out late entries: but wanna keep musuems that closed
+    ## later: should be marked as not closed should be good now: 
+    ## dt_pmyear[museum_status == "closed", max(closing), name]
+    ## when setting END_YEAR to e.g. 2015, museums that close later don't have a closing year, as intended
 
-    dt_pmdb[, .(ID, name, year_opened, year_closed, museum_status)] %>%
-        .[!is.na(year_opened)]
-        
+    ## yeet unused variables
+    dt_pmyear[, `:=`(museum_status = NULL, year_closed = NULL)]
 
     
-
+    attr(dt_pmyear, "gnrtdby") <- as.character(match.call()[[1]])
+    return(dt_pmyear)
 
 }
 
@@ -60,9 +77,19 @@ if (interactive()) {stop("it's interactive time")}
 
 END_YEAR <- 2021
 
-gd_pmyear(dt_pmdb)
+dt_pmx <- gd_pmx(dt_pmdb)
 
-## * foodweb testing
+
+dt_pmyear <- gd_pmyear(dt_pmx)
+
+library(glmmTMB)
+glmmTMB(closing ~ age + I(age^2), dt_pmyear, family = poisson) %>% summary
+
+
+
+
+
+
 
 
 
