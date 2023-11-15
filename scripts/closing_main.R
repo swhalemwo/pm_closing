825410162cec186125acd01da86917335510bc56
swhalemwo <jojo.ae@hotmail.de>
2023-11-15 11:02:28 +0100

    add average hazard calculations (via pehaz) and in-text references

--- scripts/closing_main.R
+++ scripts/closing_main.R
@@ -499,17 +499,30 @@ gt_coxzph <- function(rx) {
 
 
 
+
 ## *** nbrs generation
 gd_nbrs <- function() {
+    if (as.character(match.call()[[1]]) %in% fstd){browser()}
+    1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;
     
+    ## calculate average hazard rate numbers, depending on maximum age
+    dt_pehaz <- gd_pehaz(dt_pmcpct, cutwidth = 2)
+    dt_meanhaz <- map(c(20, 30, 40, 100), ~list(upper_bound = .x, mean_haz = dt_pehaz[cuts <= .x, mean(haz)])) %>%
+        rbindlist %>%
+        .[, .(nbr_name = sprintf("meanhaz_upto_%s", upper_bound),
+              nbr_fmt = sprintf("%s%%", format(mean_haz*100, digits = 2, nsmall = 2)),
+              grp = "meanhaz")]
+        
+    ## plots: yanking (insertion) and in-text referencing
     dt_ynkplt <- gc_ynkplt()
     dt_refplt <- gc_refplt()
 
-    dt_nbrs_cbnd <- rbind(dt_ynkplt, dt_refplt)
+    dt_nbrs_cbnd <- Reduce(rbind, list(dt_meanhaz, dt_ynkplt, dt_refplt))
 
-
+    
     return(dt_nbrs_cbnd)
 }
+
 
 
 
